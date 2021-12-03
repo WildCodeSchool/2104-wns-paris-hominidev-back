09447b89aad2430746f13ba9ec2a456340a631c1
Yann Loosli <yann@lysden.art>
2021-12-03 11:27:17 +0100

    merge dev in yann branch

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as dotenv from 'dotenv';
 import {ApolloServerPluginDrainHttpServer} from 'apollo-server-core';
-
-const {ApolloServer} = require('apollo-server-express');
-const express = require('express');
-import {Request} from "express";
+import { ApolloServer } from 'apollo-server-express';
+import express, {Request} from "express";
 import {SubscriptionServer} from 'subscriptions-transport-ws';
 import {makeExecutableSchema} from "graphql-tools";
 import {execute, subscribe} from 'graphql';
@@ -21,10 +19,14 @@ const dbConnect = require("./auth/config/config.db");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 dbConnect();
+
 (async function () {
     const app = express();
     const httpServer = createServer(app);
 
+    app.get("/", (req, res) => {
+        res.send("Pygma.Link Server is is up :)");
+      });
 
     const schema = makeExecutableSchema({
         typeDefs: type_Defs,
@@ -48,9 +50,79 @@ dbConnect();
         {server: httpServer, path: '/graphql'},
     );
 
+    const io = require('socket.io')(httpServer, {
+        handlePreflightRequest: (req: { header: { origin: any; }; }, res: { writeHead: (arg0: number, arg1: { "Access-Control-Allow-Headers": string; "Access-Control-Allow-Origin": any; "Access-Control-Allow-Credentials": boolean; }) => void; end: () => void; }) => {
+          const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin":
+              (req.header && req.header.origin) || "https://excalidraw.com",
+            "Access-Control-Allow-Credentials": true,
+          };
+          res.writeHead(200, headers);
+          res.end();
+        },
+      });
+
+      io.on("connection", (socket: { id: any; on: (arg0: string, arg1: { (roomID: any): void; (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array): void; (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array): void; }) => void; join: (arg0: any) => void; broadcast: { to: (arg0: string) => { (): any; new(): any; emit: { (arg0: string, arg1: ArrayBuffer, arg2: Uint8Array | undefined): void; new(): any; }; }; }; volatile: { broadcast: { to: (arg0: string) => { (): any; new(): any; emit: { (arg0: string, arg1: ArrayBuffer, arg2: Uint8Array): void; new(): any; }; }; }; }; }) => {
+        io.to(`${socket.id}`).emit("init-room");
+        //@ts-ignore
+        socket.on("join-room", (roomID) => {
+          console.log(`${socket.id} has joined ${roomID}`);
+          socket.join(roomID);
+          if (io.sockets.adapter.rooms[roomID].length <= 1) {
+            io.to(`${socket.id}`).emit("first-in-room");
+          } else {
+              //@ts-ignore
+              socket.broadcast.to(roomID).emit("new-user", socket.id);
+            }
+            io.in(roomID).emit(
+                "room-user-change",
+                Object.keys(io.sockets.adapter.rooms[roomID].sockets),
+                );
+            });
+            
+            socket.on(
+                "server-broadcast",
+                //@ts-ignore
+                (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+                    console.log(`${socket.id} sends update to ${roomID}`);
+                    socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
+                },
+                );
+                
+                socket.on(
+                    "server-volatile-broadcast",
+                    //@ts-ignore
+                    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+                        console.log(`${socket.id} sends volatile update to ${roomID}`);
+                        socket.volatile.broadcast
+                        .to(roomID)
+                        .emit("client-broadcast", encryptedData, iv);
+                    },
+                    );
+                    
+                    socket.on("disconnecting", () => {
+                        const rooms = io.sockets.adapter.rooms;
+                        //@ts-ignore
+                        for (const roomID in socket.rooms) {
+                            const clients = Object.keys(rooms[roomID].sockets).filter(
+                                (id) => id !== socket.id,
+                                );
+                                if (clients.length > 0) {
+                  //@ts-ignore
+                  socket.broadcast.to(roomID).emit("room-user-change", clients);
+                }
+            }
+        });
+        
+        socket.on("disconnect", () => {
+              //@ts-ignore
+            socket.removeAllListeners();
+          });
+        });
+    
     const server = new ApolloServer({
         schema,
-        cors: {origin: '*', credentials: true},
         plugins: [{
             async serverWillStart() {
                 return {
@@ -78,9 +150,15 @@ dbConnect();
 
     });
     await server.start();
-    server.applyMiddleware({app});
+    server.applyMiddleware({
+        app,
+        cors: {
+            origin: '*',
+            credentials: true
+        }
+    });
 
     httpServer.listen(PORT, () =>
-        console.log(`🚀 Server is now running on http://localhost:${PORT}/graphql`)
+        console.log(`\n🚀 GraphQl Server is now running on http://localhost:${PORT}/graphql`)
     );
 })();
