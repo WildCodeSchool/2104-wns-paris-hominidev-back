0c60f30a7ecbe6853bddb8a18d3a15ecc27a476d
OnerBerk <oner.berk@joza-it.fr>
2021-12-06 00:36:44 +0100

    update test

--- src/auth/graphQl/resolver/test/user.resolver.test.ts
+++ src/auth/graphQl/resolver/test/user.resolver.test.ts
@@ -21,20 +21,10 @@ const testUser = {
 let authenticatedUserEmail: Boolean
 
 beforeAll(async () => {
-    const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@pygmalink.oy8tj.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
-    console.log(uri)
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    }).then(() => {
-        console.log('mongo is connected');
-    });
+    dbConnect()
 })
 afterAll(async () => {
-     await mongoose.disconnect()
-
+    await mongoose.disconnect()
 })
 
 describe("user.resolver.getUsers", () => {
